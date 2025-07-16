ad8f4c2a5c024a768ac1f85615dc1d45940d4f17
Joran <joran.lequellec@gmail.com>
2025-07-16 22:00:12 +1200

    lorem text about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,9 +9,26 @@ export default function About(): React.ReactElement {
       <main className="flex-grow w-3/4 mt-26">
         <ImageBlock alt="about" src="/about.jpg" />
 
-        <p className="text-lg mb-6 h-[2000px]">
+        <p className="text-2xl text-center mb-2">
           We are a barbershop quartet based in Christchurch, New Zealand,
           dedicated to bringing joy through music.
+        </p>
+
+        <p className="font-light mb-6">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+          aspernatur aliquam eius quod unde, nemo qui, autem, nulla asperiores
+          consectetur esse architecto? Perspiciatis commodi, ullam rem iusto
+          voluptatum modi hic? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Et quae amet eaque, neque at dolor. Vero commodi
+          officiis temporibus quo ut labore atque alias ab. Ad voluptas ipsam
+          minima incidunt? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Ipsa pariatur repudiandae similique laudantium ipsam architecto
+          mollitia libero eveniet sint beatae, ducimus quasi accusantium rem
+          repellendus ratione obcaecati, sequi dolor voluptatem. Lorem ipsum
+          dolor sit amet consectetur adipisicing elit. Ipsa pariatur repudiandae
+          similique laudantium ipsam architecto mollitia libero eveniet sint
+          beatae, ducimus quasi accusantium rem repellendus ratione obcaecati,
+          sequi dolor voluptatem.
         </p>
       </main>
     </div>
